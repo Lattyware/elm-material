746f8378996d306f2153003dc3803c446bd190c3
Gareth Latty <gareth@lattyware.co.uk>
2020-05-27 22:43:22 +0100

    Add text field handling for return key presses.

--- src/elm/Material/TextField.elm
+++ src/elm/Material/TextField.elm
@@ -2,11 +2,13 @@ module Material.TextField exposing
     ( Type(..)
     , view
     , viewWithFocus
+    , viewWithReturn
     )
 
 import Html exposing (Html)
 import Html.Attributes as HtmlA
 import Html.Events as HtmlE
+import Json.Decode as Json
 import Material.Attributes as HtmlA
 
 
@@ -38,6 +40,19 @@ view label t value action =
 viewWithFocus : String -> Type -> String -> Maybe (String -> msg) -> msg -> msg -> Html msg
 viewWithFocus label t value action onFocus onBlur =
     viewWithAttrs label t value action [ onFocus |> HtmlE.onFocus, onBlur |> HtmlE.onBlur ]
+
+
+viewWithReturn : String -> Type -> String -> Maybe (String -> msg) -> msg -> msg -> Html msg
+viewWithReturn label t value action onReturn noOp =
+    let
+        onKeyPress k =
+            if k == 13 then
+                onReturn
+
+            else
+                noOp
+    in
+    viewWithAttrs label t value action [ HtmlE.keyCode |> Json.map onKeyPress |> HtmlE.on "keydown" ]
 
 
 
